e25d65bf80fc5e99a2df5b0a2a2ff9ca06e14a89
appbat <appbat2017@gmail.com>
2017-05-15 19:00:08 -0500

    data de pedidos

--- app/scripts/controllers/pedidos.js
+++ app/scripts/controllers/pedidos.js
@@ -13,47 +13,88 @@ angular.module('skinkApp')
     var ref = firebase.database().ref().child("VisitaVenta");
     $scope.pedidosData = $firebaseArray(ref);
     console.log($scope.pedidosData);
+     $scope.pedidosData.$loaded().then(function() {
+          
+
+
+            for (var j = 0; j <  $scope.pedidosData.length ; j++) {
+                console.log("for");
+        
+                $scope.pedidosData[j].editp = false ; 
+              }
+
+        });
+
+  
 
      $scope.formatDate = function(date){
           var dateOut = new Date(date);
           return dateOut;
     };
 
-     for (var i = 0; i <  $scope.pedidosData.length ; i++) {
-    	$scope.pedidosData[i].edit = false ; 
-    }
 
     $scope.edit = function(id ,uid){
-    		if( $scope.pedidosData[id].edit){
-    			 $scope.pedidosData[id].edit = false ; 
-    			 if($scope.pedidosData[id].entregado === undefined){
- 						$scope.pedidosData[id].entregado  = false ;
-    			 }
-           if($scope.pedidosData[id].guia === undefined || $scope.pedidosData[id].guia === null){
-              $scope.pedidosData[id].guia  =" ";
 
-           }
-    			 ref.child(uid).update(
-    				{
+    
 
-    					total : $scope.pedidosData[id].total,
-    					guia : $scope.pedidosData[id].guia ,
-    					numeroFactura: $scope.pedidosData[id].numeroFactura ,
-    					entregado : $scope.pedidosData[id].entregado  
-    					
-    				}
-    			);
-    		}else{
-    			$scope.pedidosData[id].edit = true ; 
-    		}
+
+       console.log("entra " + id + " uid  = " + uid);
+        for (var j = 0; j <  $scope.pedidosData.length ; j++) {
+
+            if($scope.pedidosData[j].$id === uid){
+               console.log("encontro igual");
+
+              if( $scope.pedidosData[j].editp){
+
+                  $scope.pedidosData[j].editp = false ; 
+                  if($scope.pedidosData[j].entregado === undefined){
+                    $scope.pedidosData[j].entregado  = false ;
+                 }
+                  if($scope.pedidosData[j].guia === undefined || $scope.pedidosData[j].guia === null){
+                     $scope.pedidosData[j].guia  =" ";
+
+                  }    
+                  ref.child(uid).update(
+                    {
+
+                      total : $scope.pedidosData[j].total,
+                      guia : $scope.pedidosData[j].guia ,
+                      numeroFactura: $scope.pedidosData[j].numeroFactura ,
+                      entregado : $scope.pedidosData[j].entregado  
+              
+                    }
+                  );
+                }else{
+            
+                 $scope.pedidosData[j].editp = true ; 
+                 console.log("activar" +   $scope.pedidosData[j].editp);
+                }
+
+
+            }
+               
+        }
+
+
+
+
        
 
     }
     //$scope.dataProductos = [];
     $scope.detallePedido = function(id){
-        console.log("entra");
-        $scope.dataProductos  =$scope.pedidosData[id].productos ; 
-        console.log($scope.dataProductos);
+
+
+         for (var j = 0; j <  $scope.pedidosData.length ; j++) {
+             if($scope.pedidosData[j].$id === id){
+                      console.log("entra");
+                     $scope.dataProductos  =$scope.pedidosData[j].productos ; 
+                      console.log($scope.dataProductos);
+             }
+
+         }
+
+      
        /* for (var i = 0; i < $scope.dataProducts.length; i++) {
            // $scope.dataProducts[id].descripcion = "" ;
             console.log($scope.dataProducts[i].idProducto);
